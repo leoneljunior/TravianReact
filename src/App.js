9b208a2cb5b6548ffcabc4ecf5e7cdbd240ba235
leoneljunior <junior.hpj@hotmail.com>
2021-03-27 03:24:09 -0300

    First component (Table)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import './App.css';
+import {MyTable} from './components/myTable';
 
 export class App extends React.Component {
     render(){
-      return <div>Junior </div>;
+      return (
+        <div>
+          <div id="header">
+            <img id="logo" alt="logo" src="logo.png" /><br/>
+            PB War Portal
+          </div>
+
+          <div id="body"></div>
+
+          <hr />
+          <div>
+            {/* <Header /> */}
+            {/* <MyFilters /> */}
+            <MyTable />
+            <MyTable />
+          </div>
+        </div>
+      );
     }
   };
 
